fdcc29211ec955d53909356e5819ac8c334ecb3c
SergioGBosco <sergiogibosco@gmail.com>
2025-09-09 16:26:24 +0200

    ffeat: add button on navbar

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+
 const Header = () => {
+
+  const ActiveMode = () => {
+    return (console.log("ciao"))
+  }
   return (
-    <div>
+    <header className='m-3'>
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <ul className='list-unstyled d-flex'>
-              <li>
-                <NavLink to={"/"} className="nav">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/info"} className="nav" >Info</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/prodotti"} className="nav" >Prodotti</NavLink>
-              </li>
-            </ul>
+          <div className="col-12 d-flex justify-content-between">
+            <div>
+              <ul className='list-unstyled d-flex'>
+                <li>
+                  <NavLink to={"/"} className="nav">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/info"} className="nav" >Info</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/prodotti"} className="nav" >Prodotti</NavLink>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button className='btn btn-success' onClick={ActiveMode} >Modalità Budget</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </header>
   )
 }
 
